09f2dc74c515aa99841331fc5cd6fa5da8a4d712
Hussains3 <hussainasabbir3@gmail.com>
2023-06-28 19:58:04 +0600

    Frontend Pages auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -49,4 +49,98 @@
             </x-primary-button>
         </div>
     </form>
+    <!-- ==============Team start========== -->
+    <section>
+        <div class=" bg-lgreen pb-24  pt-10  lg:py-44 px-3 lg:px-0 ">
+            <div class="max-w-7xl mx-auto   px-3 md:px-0 slider_gap">
+                <div class="grid lg:grid-cols-2 gap-5 lg:gap-20">
+                    <div class="">
+                        <img class=" w-full" src="./img/singin.jpg" alt="Signin cover">
+                    </div>
+                    <div class="">
+                        <div class="">
+                            <h2 class="text-2xl lg:text-4.5xl text-nblue font-bold text-center uppercase">Welcome
+                            </h2>
+                            <p class="text-base font-normal text-center text-nblue mt-2">Please enter your details to
+                                register</p>
+                        </div>
+
+                        <div class=" mt-20">
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+                                <div class=" flex items-center signin_line mt-7">
+                                    <div class="">
+                                        <p class=" text-2xl text-nblue mt-2"><iconify-icon
+                                                icon="mdi:user"></iconify-icon>
+                                        </p>
+                                    </div>
+                                    <div class="">
+                                        <input
+                                            class=" text-lg text-nblue  bg-transparent w-full px-4 py-2 focus:outline-none"
+                                            type="text" placeholder="Full name">
+                                    </div>
+                                </div>
+                                <div class=" flex items-center signin_line mt-7">
+                                    <div class="">
+                                        <p class=" text-2xl text-nblue mt-2"><iconify-icon icon="ic:round-email"></iconify-icon>
+                                        </p>
+                                    </div>
+                                    <div class="">
+                                        <input
+                                            class=" text-lg text-nblue  bg-transparent w-full px-4 py-2 focus:outline-none"
+                                            type="text" placeholder="Email Address">
+                                    </div>
+                                </div>
+                                <div class=" flex items-center signin_line mt-7">
+                                    <div class="">
+                                        <p class=" text-2xl text-nblue mt-2"><iconify-icon
+                                                icon="material-symbols:lock"></iconify-icon>
+                                        </p>
+                                    </div>
+                                    <div class="">
+                                        <input
+                                            class=" text-lg text-nblue  bg-transparent w-full px-4 py-2 focus:outline-none"
+                                            type="password" placeholder="Password">
+                                    </div>
+                                </div>
+                                <div class=" flex items-center signin_line mt-7">
+                                    <div class="">
+                                        <p class=" text-2xl text-nblue mt-2"><iconify-icon
+                                                icon="material-symbols:lock"></iconify-icon>
+                                        </p>
+                                    </div>
+                                    <div class="">
+                                        <input
+                                            class=" text-lg text-nblue  bg-transparent w-full px-4 py-2 focus:outline-none"
+                                            type="password" placeholder="Confirm Password">
+                                    </div>
+                                </div>
+                                <div class=" mt-5 flex justify-between items-center">
+                                    <div class=" flex items-center">
+                                        <div class=" mr-2">
+                                            <input class="" type="checkbox">
+                                        </div>
+                                        <div class="">
+                                            <p class="text-sm md:text-base text-nblue">I have accepted to the <a href="termscondition.html" class=" text-dgreen">terms and conditions</a></p>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="mt-12">
+                                    <button type=" submit"
+                                        class=" text-white w-full bg-dgreen py-2.5 px-5 text-base font-bold tracking-wider rounded-md">SIGN
+                                        IN</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="mt-3">
+                            <p class=" text-base text-nblue text-center">Already Have an account? <a href="{{ route('login') }}" class=" text-dgreen"> Sign In</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ==============Team start========== -->
 </x-guest-layout>
