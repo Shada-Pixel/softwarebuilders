18aa1190d987fbdc61c4baa9b0b517775fc90e39
Hussains3 <hussainasabbir3@gmail.com>
2023-08-12 21:25:51 +0600

    Welcome mail

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,15 +5,18 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\View\View;
+use App\Mail\WelcomeMail;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Auth\Events\Registered;
 use App\Providers\RouteServiceProvider;
 use Spatie\Permission\Models\Permission;
+
 
 
 class RegisteredUserController extends Controller
@@ -58,6 +61,23 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
         Auth::login($user);
+
+
+        $msg = 'Welcome to Software Builders Ltd. Your acount has been created.';
+        $link= route('home');
+        $maildata = ['name' => $user->name, 'text' => $msg , 'link' => $link ];
+        $sendmail = Mail::to($user->email)->send(new WelcomeMail($maildata));
+
+        // Trying to send Welcome
+        try {
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+
+
+
+
+
         return redirect(RouteServiceProvider::HOME);
     }
 }
